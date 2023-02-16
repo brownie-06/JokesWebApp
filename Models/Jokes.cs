6fa1e92faad013691079c35775d42d424a2dc74d
Srishti.Srivastava <Srishti.Srivastava@CMLPT443>
2023-02-16 19:27:42 +0530

    Add project files.

---
+++ Models/Jokes.cs
@@ -0,0 +1,13 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        public Jokes()
+        {
+
+        }
+    }
+}
